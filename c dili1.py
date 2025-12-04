9ab58514cb98eb31efb7bc31d89f9a39df2cd0f1
ghdg73706 <5ntjd2nfhb@privaterelay.appleid.com>
2025-12-05 02:37:21 +0300

    Create c dili1.py

---
+++ c dili1.py
@@ -0,0 +1,23 @@
+printf("merhaba dünya");
+return 0;
+printf("hello world!"\n);
+return 0;
+// yorum yapmak için kullanılır
+/*program başına ve sonuna koyarsak program çalışmaz*/
+
+\n alt alta yazdırır=
+printf("adınız:elif\n , soyadınız:kubur\n");  = kodu okumak zor olur bunun yerine;
+printf("adınız:elif\n")
+printf(soyadınız:kubur\n")
+
+ESCAPE SEQUENCES:
+\n = yeni satır
+\t = yatay sekme
+\b = Backspace tusunu işleve alır
+\" = cift tırnak işareti basar
+\\ =ters bölü ekrana yazdırır
+
+printf("adınız:elif \b gaye\n") burada elif yazısını kaldırırdı
+printf(soyadınız\t:kubur\n") //boşluk bırakır
+printf(" elif \"geç\" kaldı") tırnak içinde yazdırır
+
